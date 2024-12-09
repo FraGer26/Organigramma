b6a27cc465c4188425a08544ba66e52f3e359e1c
FraGer26 <gervinofrancesco@gmail.com>
2024-12-09 17:16:46 +0100

    test2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,22 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
- System.out.println("Hello World!");
+     JFrame frame = new JFrame();
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.setSize(800, 600);
+     frame.setVisible(true);
+     JPanel panel = new JPanel();
+     frame.add(panel);
+     panel.setLayout(null);
+     JButton button1 = new JButton("Button1");
+     panel.add(button1);
+     JButton button2 = new JButton("Button2");
+     panel.add(button2);
+     JButton button3 = new JButton("Button3");
+     panel.add(button3);
 
     }
 }
